789b390b5af23a47594dcb91fd42b6dda77ce791
Stephen Turner <slturner@gmail.com>
2026-04-26 23:02:13 -0500

    feat: localize index storage by using absolute path for DEFAULT_DATA_DIR

--- tiny_file_router/const.py
+++ tiny_file_router/const.py
@@ -2,7 +2,10 @@ import os
 from pathlib import Path
 
 DEFAULT_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-DEFAULT_DATA_DIR = Path(os.environ.get("TINY_ROUTER_DATA_DIR", "router_data"))
+DEFAULT_DATA_DIR = Path("router_data").absolute()
 DEFAULT_MAX_CHARS = int(os.environ.get("TINY_ROUTER_CHUNK_MAX_CHARS", "900"))
 DEFAULT_OVERLAP_SENTENCES = int(os.environ.get("TINY_ROUTER_OVERLAP_SENTENCES", "1"))
 HOME_DATA_DIR = Path.home() / ".tiny_file_router"
+
+if "TINY_ROUTER_DATA_DIR" in os.environ:
+    DEFAULT_DATA_DIR = Path(os.environ["TINY_ROUTER_DATA_DIR"]).absolute()
